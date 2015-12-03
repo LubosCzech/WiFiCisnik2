f5b6738e4feb7b9865e8b872db4a81a5cfc862d3
Luboš Světik <lubos.svetik@gmail.com>
2015-12-03 15:54:52 +0100

    min

--- tools/min/run.php
+++ tools/min/run.php
@@ -18,4 +18,11 @@ $minified_js = PHPWee\Minify::js($autorefresh);
 file_put_contents(getcwd().'/webroot/js/autorefresh.min.js',$minified_js);
 
 
+$default = file_get_contents(getcwd().'/src/Template/Layout/default.ctp');
+$str=str_replace("autorefresh.js", "autorefresh.min.js",$default);
+file_put_contents(getcwd().'/src/Template/Layout/default.ctp',$str);
+
+
+
+
 
