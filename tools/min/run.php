3e0faafa6f01a258864fe7e708e23e021da4a0e6
Luboš Světik <lubos.svetik@gmail.com>
2015-12-03 15:38:38 +0100

    min

--- tools/min/run.php
+++ tools/min/run.php
@@ -9,6 +9,7 @@
 require_once ('phpwee.php');
 
 $custom_css = file_get_contents(getcwd().'/webroot/css/custom.css');
+echo $custom_css;
 $minified_css = PHPWee\Minify::css($custom_css);
 file_put_contents(getcwd().'/webroot/css/custom.min.css',$minified_css);
 echo $minified_css;
