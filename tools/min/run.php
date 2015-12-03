aeb217bc839a01822031be6c2fdd3a7557e3cee9
Luboš Světik <lubos.svetik@gmail.com>
2015-12-03 14:49:51 +0100

    Min Tool

---
+++ tools/min/run.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lubossvetik
+ * Date: 03.12.15
+ * Time: 14:45
+ */
+
+require_once ("phpwee.php");
+
+$custom_css = file_get_contents("../../webroot/css/custom.css");
+$minified_css = PHPWee\Minify::css($custom_css);
+file_put_contents("../../webroot/css/custom.min.css",$minified_css);
